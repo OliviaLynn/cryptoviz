8a02964db665881a53d0589d650cb2a482dd44ec
Olivia Lynn <olynn@andrew.cmu.edu>
2021-10-12 13:15:01 -0400

    init

---
+++ interactive-blockchain.js
@@ -0,0 +1,56 @@
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return sha256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "01/01/2021", 0, "");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock); //irl this wouldn't be so easy, there are numerous checks in place
+    }
+
+    prettyPrint() {
+        return "Is valid? " + this.isChainValid() + "\n" + JSON.stringify(this, null, 4);
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+var myCoin = new Blockchain();
+
+myCoin.addBlock(new Block(1, "10/11/2021", 4));
+myCoin.addBlock(new Block(2, "10/13/2021", 10));
